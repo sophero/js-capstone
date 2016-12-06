9062ee7e99495cde1fd4cee43a0a8d5d6b5a5861
Sophia Di <backwardsxmarathon@gmail.com>
2016-12-06 15:48:08 -0500

    Began writing searchLetter method. Changed printSpaces method to work character by character instead of word by word.

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
 
 
-// function makePhrase(string) {
-// 	var phrase = string.toUpperCase();
-// 	var splitPhrase = phrase.split(" ");
-// 	var numWords = splitPhrase.length;
-
-// 	for (let k = 0; k  < numWords; k++) {
-// 	$(".phrase-container").html()
-
-// 		// splitPhrase[k].length
-
-// 	}
-
-// 	// Send to another function to print the empty spaces?
-// }
-
-function Hangman() {
+function HangmanGame() {
 
 	// Take an argument in the form of an array containing strings for use in the game.
 
-	// Set default phrase.
-	this.phrase = "";
+	// Set current phrase.
+	this.currentPhrase = "";
 
-	this.printSpaces = function(string) {
+	this.incorrectGuesses = 0;
+
+	this.printSpaces = function(string, arrayIndicesToPrint) {
 
 		// Check for only letters and spaces in the string.
 		var letters = /^[a-zA-Z\s]+$/;
+
 		if (!letters.test(string)) {
 			alert("Phrase contains invalid characters - letters and spaces only please.");
 		}
@@ -33,26 +21,84 @@ function Hangman() {
 		// Reset phrase container on page.
 		$(".phrase-container").html("");
 
-		// Split phrase into separate words.
-		var splitPhrase = string.split(" ");
-		var numWords = splitPhrase.length;
+		// // Split phrase into separate words. This isn't as versatile as doing it character by character..
+		// var splitPhrase = string.split(" ");
+		// var numWords = splitPhrase.length;
 
-		for (let k = 0; k < numWords; k++) {
-			var numLetters = splitPhrase[k].length;
 
-			$(".phrase-container").append("_ ".repeat(numLetters));
-			$(".phrase-container").append("<br>");
+		// // Print underscores to page.
+		// for (let k = 0; k < numWords; k++) {
+		// 	var numLetters = splitPhrase[k].length;
 
+		// 	$(".phrase-container").append("_ ".repeat(numLetters));
+		// 	$(".phrase-container").append("<br>");
+
+		// }
+
+		// Split phrase character by character.
+		var charArray = string.split("");
+
+		// Print underscores to page.
+		for (let k = 0; k < charArray.length; k++) {
+
+			if (charArray[k] === " ") {
+
+				$(".phrase-container").append("<br>");
+
+			} else {
+
+				$(".phrase-container").append("_ ");
+
+			}
 		}
 
-		this.phrase = string.toUpperCase();
+
+		// Set current phrase on HangmanGame object.
+		this.currentPhrase = string.toUpperCase();
+
+		return "Current phrase set to: " + this.currentPhrase;
 	}
 
 
-	function searchLetter(letter) {
+	this.searchLetter = function(letter) {
+
+		var er = letter.toUpperCase();
+		var charArray = this.currentPhrase.split("");
+		
+		var matchedLetter = false;
+		var indicesToReplace = [];
+
+		for (let k = 0; k < charArray.length; k++) {
+
+			if (er === charArray[k]) {
+
+				// Make that letter appear on the screen?
+				// Nah, add that index to indices to replace.
+				indicesToReplace.push(k)
+
+				matchedLetter = true;
+			}
+
+		} 
+
+		if (matchedLetter) {
+
+			// Add the letter to each letter to replace
+			console.log(indicesToReplace);
+
+		} else {
+
+			// Set up a hangman dude. Incorrect guesses += 1
+			this.incorrectGuesses++;
+
+			return "Not a match!"
+
+		}
+
+		
 
 	}
 
 }
 
-var hangman = new Hangman();
+var hangman = new HangmanGame();
